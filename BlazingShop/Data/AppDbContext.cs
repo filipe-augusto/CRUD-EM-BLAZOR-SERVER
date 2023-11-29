dc830842a922e8f053d8a71d8f44908dc4ad7324
Filipe Augusto Santos Chaves <67299107+filipe-augusto@users.noreply.github.com>
2023-11-29 09:50:20 -0300

    Adicionar arquivos de projeto.

---
+++ BlazingShop/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BlazingShop.Models;
+namespace BlazingShop.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+     : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+    }
+}
